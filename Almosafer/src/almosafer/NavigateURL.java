565366efd353f2ebf9bed982cb5b32b425f5b405
alsaw <alsaw@DESKTOP-11MDM02>
2024-05-13 17:22:56 +0300

    update 2

--- Almosafer/src/almosafer/NavigateURL.java
+++ Almosafer/src/almosafer/NavigateURL.java
@@ -1,5 +1,23 @@
 package almosafer;
 
-public class NavigateURL {
+import org.testng.annotations.Test;
+
+public class NavigateURL extends Setup {
+
+
+	@Test(priority = 8)
+	public void CheakNavigateURL() throws InterruptedException {
+
+
+		String acualUrl = driver.getCurrentUrl();
+
+
+		String expectedUrl = "https://global.almosafer.com/en";
+		myassert.assertEquals(acualUrl, expectedUrl);
+		myassert.assertAll();
+
+
+	}
+
 
 }
--- Almosafer/src/almosafer/Random_Language.java
+++ Almosafer/src/almosafer/Random_Language.java
@@ -1,5 +1,57 @@
 package almosafer;
 
-public class Random_Language {
+import java.util.Random;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+public class Random_Language extends Setup
+{
+
+	@Test(priority = 10)
+	public void CheakRandomlanguage() throws InterruptedException {
+
+		Random random = new Random();
+		int number=random.nextInt(2);
+		System.out.println(number);
+
+		if(number==0) {
+			Thread.sleep(4000);
+
+		WebElement locateLanguageAR	= driver.findElement(By.xpath("//a[contains(text(),'العربية')]"));
+			javaScript(locateLanguageAR);
+			locateLanguageAR.click();
+			Thread.sleep(10000);
+			WebElement locateaculText = driver.findElement(By.xpath("/html/body/div[2]/section[2]/div[4]/div/div/nav/a[1]/div"));
+		    String acualText = locateaculText.getText();
+		    System.out.println(acualText);
+		    String expectText="طيران";
+			myassert.assertEquals(acualText, expectText);
+
+		}
+
+		else {
+			driver.get("https://www.almosafer.com/ar?ncr=1");
+			Thread.sleep(4000);
+			WebElement locateLanguageEN=	driver.findElement(By.cssSelector("a[data-testid=\"Header__LanguageSwitch\"]"));
+
+			javaScript(locateLanguageEN);
+			locateLanguageEN.click();
+			Thread.sleep(10000);
+			WebElement locateaculText = driver.findElement(By.xpath("/html/body/div[2]/section[2]/div[4]/div/div/nav/a[1]/div"));
+		    String acualText = locateaculText.getText();
+		    String expectText="Flights";
+			myassert.assertEquals(acualText, expectText);
+
+
+		}
+		myassert.assertAll();
+
+	}
+
+
+
+
 
 }
